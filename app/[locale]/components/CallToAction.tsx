0625d2d2ecc466785e6edade94e3e1a14bf9ff0f
leelongxi <leelongxi@foxmail.com>
2025-05-07 21:27:39 +0800

    Init

--- app/[locale]/components/CallToAction.tsx
+++ app/[locale]/components/CallToAction.tsx
@@ -2,13 +2,18 @@
  * @Author: leelongxi leelongxi@foxmail.com
  * @Date: 2025-05-05 19:29:08
  * @LastEditors: leelongxi leelongxi@foxmail.com
- * @LastEditTime: 2025-05-05 19:29:12
- * @FilePath: /24k-finance-website/app/components/CallToAction.tsx
+ * @LastEditTime: 2025-05-07 21:20:22
+ * @FilePath: /24k-finance-website/app/[locale]/components/CallToAction.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+'use client';
+
 import React from 'react';
+import { useTranslations } from 'next-intl';
 
 const CallToAction: React.FC = () => {
+  const t = useTranslations('cta');
+
   return (
     <div className="relative rounded-xl overflow-hidden bg-[#1c1c24]/80 backdrop-blur-sm p-12 my-16 shadow-lg">
       {/* 背景视觉效果 */}
@@ -16,14 +21,14 @@ const CallToAction: React.FC = () => {
 
       <div className="relative z-10 text-center text-white">
         <h2 className="text-3xl md:text-4xl font-bold mb-6 leading-tight">
-          是时候加入成千上万的
-          <br className="hidden sm:block" /> {/* 在小屏幕上换行 */}
-          创作者、艺术家和开发者们，
-          <br className="hidden sm:block" /> {/* 在小屏幕上换行 */}
-          一起使用 24K 了。
+          {t('title1')}
+          <br className="hidden sm:block" />
+          {t('title2')}
+          <br className="hidden sm:block" />
+          {t('title3')}
         </h2>
         <button className="bg-purple-600 hover:bg-purple-700 text-white font-semibold px-6 py-2 rounded-full transition-colors duration-300">
-          立即认购
+          {t('button')}
         </button>
       </div>
     </div>
--- app/[locale]/utils/index.ts
+++ app/[locale]/utils/index.ts
@@ -2,7 +2,7 @@
  * @Author: leelongxi leelongxi@foxmail.com
  * @Date: 2025-05-07 17:36:24
  * @LastEditors: leelongxi leelongxi@foxmail.com
- * @LastEditTime: 2025-05-07 20:55:50
+ * @LastEditTime: 2025-05-07 21:26:01
  * @FilePath: /24k-finance-website/app/utils/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,6 +23,8 @@ export async function getMessages(locale: string) {
     miningPlatform: (await import(`@/locales/${locale}/miningPlatform.json`)).default,
     footers: (await import(`@/locales/${locale}/footers.json`)).default,
     common: (await import(`@/locales/${locale}/common.json`)).default,
+    cta: (await import(`@/locales/${locale}/cta.json`)).default,
+    kyc: (await import(`@/locales/${locale}/kyc.json`)).default,
   };
   
   return messages;
