bcdcd289f7d43b2e0368b6d1778cdcda6761b5d3
leelongxi <leelongxi@foxmail.com>
2025-05-10 10:07:21 +0800

    Init

--- app/[locale]/components/CallToAction.tsx
+++ app/[locale]/components/CallToAction.tsx
@@ -2,7 +2,7 @@
  * @Author: leelongxi leelongxi@foxmail.com
  * @Date: 2025-05-05 19:29:08
  * @LastEditors: leelongxi leelongxi@foxmail.com
- * @LastEditTime: 2025-05-08 18:17:28
+ * @LastEditTime: 2025-05-10 10:06:10
  * @FilePath: /24k-finance-website/app/[locale]/components/CallToAction.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,7 +20,9 @@ const CallToAction: React.FC = () => {
       <div className="absolute inset-0 opacity-20 bg-gradient-to-tr from-purple-600 via-teal-500 to-indigo-600 blur-3xl -z-10"></div>
 
       <div className="relative z-10 text-center text-white">
-        <h2 className="text-3xl md:text-4xl font-bold mb-6 leading-tight">
+        <h2 className="text-3xl md:text-4xl font-bold mb-6 leading-tight font-sans"  style={{
+          fontFamily: 'auto'
+        }} >
           {t('title1')}
           <br className="hidden sm:block" />
           {t('title2')}
